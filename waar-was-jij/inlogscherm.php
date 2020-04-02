cea320d7ec03158b8607f136aa9a6d1de90b958c
Fabian <19959@ma-web.nl>
2020-04-02 18:46:24 +0200

    add menu

--- waar-was-jij/inlogscherm.php
+++ waar-was-jij/inlogscherm.php
@@ -100,11 +100,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/inlogscherm.css">
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Waar was jij?</title>
 </head>
 
 <body>
     <nav>
+        <input type="checkbox" id="menuSwitcher" checked>
+        <div class="menu">
+            <label class="close-menu" for="menuSwitcher"><i class="fa fa-bars"></i></label>
+            <ul>
+                <li>
+                    <a href="home.php">Home</a>
+                </li>
+                <li>
+                    <a href="foto-upload.php">Uploaden</a>
+                </li>
+                <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) : ?>
+                    <li>
+                        <a href="gebruikers-pagina.php">Mijn pagina</a>
+                    </li>
+                <?php endif ?>
+                <li>
+                    <a href="register.php">Registreren</a>
+                </li>
+                <li>
+                    <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) : ?>
+                        <a href="logout.php">Uitloggen</a>
+                    <?php else : ?>
+                        <a class="active" href="inlogscherm.php">Inloggen</a>
+                    <?php endif ?>
+                </li>
+            </ul>
+        </div>
+        <label for="menuSwitcher" class="haal-menu"><i class="fa fa-bars"></i></label>
         <h1>Waar was jij?</h1>
         <img src="img/waar_was_jij_logo.png" alt="Logo">
     </nav>
